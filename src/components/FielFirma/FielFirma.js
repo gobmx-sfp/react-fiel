3c898f9b27c782990f759b0b07e2d2964d47accb
David Regla <dreglad@gmail.com>
2020-04-15 10:19:34 -0500

    typo

--- src/components/FielFirma/FielFirma.js
+++ src/components/FielFirma/FielFirma.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import {
   CircularProgress,
   FormLabel,
@@ -60,7 +59,7 @@ function FielFirma({ cadena, proxyUrl, onFirma }) {
 
   useEffect(() => {
     if (firma) {
-      setFirma(firma);
+      onFirma(firma);
     }
   }, [firma]);
 
